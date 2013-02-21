a0ca7d5a999dff7592a7b4b14d279d1cb0717fda
keiono <keiono@gmail.com>
2013-02-21 11:46:50 +0900

    Some commments had been added.

--- src/main/java/lu/lcsb/garuda/GarudaEventHandler.java
+++ src/main/java/lu/lcsb/garuda/GarudaEventHandler.java
@@ -2,10 +2,28 @@ package lu.lcsb.garuda;
 
 import jp.sbi.garuda.client.backend.listeners.GarudaBackendPropertyChangeEvent;
 
+/**
+ * Public API for Garuda event handler.
+ * 
+ *
+ */
 public interface GarudaEventHandler {
 	
+	/**
+	 * Returns name of event can be handled by this handler.
+	 * 
+	 * @return pre-defined event name by Garuda Alliance.  See (link to their doc)
+	 */
 	public String getCompatibleEventType();
 	
+	/**
+	 * Actual event handler.  Based on the objects in the event, handler will 
+	 *  - Load network
+	 *  - Export table
+	 *  - etc.
+	 *  
+	 * @param event event object from Garuda core.
+	 */
 	public void handleEvent(final GarudaBackendPropertyChangeEvent event);
 
 }
